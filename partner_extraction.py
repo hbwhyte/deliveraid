3eb1a3192d3d2eee656a2f3a27bef1ce9d84011f
hbwhyte <heather.b.whyte@gmail.com>
2019-07-07 12:49:13 +0100

    Add csv extractions for data visualizations

--- partner_extraction.py
+++ partner_extraction.py
@@ -1,6 +1,10 @@
+import csv
+
 with open("PMNCH_partners_raw.txt") as f_in:
     lines = filter(None, (line.rstrip() for line in f_in))
+
 lst = []
+partners = []
 addNext = False
 addLast = False
 
@@ -16,6 +20,18 @@ class Partner:
     def __getitem__(self, key):
         if isinstance(key, int):
             return key
+
+    def partnerList(self):
+        pl = [self.name, self.constituency, self.focus, self.country, self.region]
+        return pl
+
+    def countryList(self):
+        cl = [self.name, self.country]
+        return cl
+
+    def choropleth(self):
+        choro = [self.name, self.country]
+        return choro
 
 
 for line in lines:
@@ -38,5 +54,26 @@ for line in lines:
         focus = lst[2]
         country = lst[3]
         partner = Partner(name, constituency, focus, country, region)
+        partners.append(partner)
         del lst[:]
         addLast = False
+
+choro = {}
+for partner in partners:
+    if partner.country in choro:
+        choro[partner.country] += 1
+    else:
+        choro[partner.country] = 1
+
+with open('countries.csv', 'wb') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    for partner in partners:
+        wr.writerow(partner.countryList())
+
+with open('countries_choro.csv', 'wb') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    for ch in choro:
+        concat = ch + "," + str(choro[ch])
+        print concat
+        wr.writerow([ch])
+        wr.writerow([choro[ch]])

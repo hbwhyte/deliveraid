1ca2feabbdcf8ad5a9366c4272490166c8fe3041
Charles <36535592+charlesghost@users.noreply.github.com>
2019-07-07 12:59:22 +0100

    more stuff

--- twiliotest/sms_bot.py
+++ twiliotest/sms_bot.py
@@ -1,28 +1,32 @@
+# import all the libraries we will be using
+
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
+# set up Flask to connect this code to the local host, which will
+# later be connected to the internet through Ngrok
 app = Flask(__name__)
 
-
-@app.route('/sms', methods=['GET', 'POST'])
+# Main method
+@app.route('/sms', methods=['POST'])
 def sms_reply():
-
+    # twilio response object
     resp = MessagingResponse()
+
+    # Get text in message sent
     body = request.form['Body']
-    if "HELP" in body:
+
+    # Case Statement showing all the reponse options
+    if body == 'HELP':
         resp.message("Thank you for your response! We are going to find some help for you!")
-    elif "SUPPORT" in body:
+    elif body == 'SUPPORT':
         resp.message("Support Message here")
     else:
-        resp.message("\n Welcome to DeliverAid! These are the commands you may use: \nHELP "
-                     "\"get help from one of our partners\" \nSUPPORT \"support request\"\nOTHER \"place\""
-                     "\nSOME_KEYWORD \"some custom request\"\n")
+        resp.message("\n Welcome to DeliverAid! \nThank you, "
+                     "\"can you tell us where you are now so we can find you!\"\n")
 
     return str(resp)
 
 
 if __name__ == '__main__':
     app.run()
-
-
-
